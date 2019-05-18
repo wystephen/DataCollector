91149ce9d01e54bf73a7eefdda539d7f0582a059
steve <551619855@qq.com>
2019-05-18 14:51:09 +0800

    new

--- myntreader.cpp
+++ myntreader.cpp
@@ -1,2 +1,68 @@
 #include "myntreader.h"
 
+MYNTEYE_USE_NAMESPACE
+
+MYNTReader::MYNTReader(){
+    DeviceInfo dev_info;
+    bool cam_ok = selectDevice(&dev_info);
+    if(cam_ok){
+        OpenParams cam_params(dev_info.index);
+
+        cam_params.framerate = 20;
+        cam_params.stream_mode = StreamMode::STREAM_2560x720;
+
+        cam_params.color_mode = ColorMode::COLOR_RAW;
+        //        cam_params.depth_mode = DepthMode::DEPTH_GRAY;
+        cam_params.depth_mode = DepthMode::DEPTH_COLORFUL;
+
+        cam_params.ir_depth_only = false;
+
+        cam_params.ir_intensity = 0; // close IR
+
+        mynt_cam_.EnableImageInfo(true);
+        mynt_cam_.EnableMotionDatas();
+        mynt_cam_.Open(cam_params);
+
+    }
+}
+
+
+bool MYNTReader::selectDevice(DeviceInfo *dev_info) {
+    std::vector<DeviceInfo> dev_infos = mynt_cam_.GetDeviceInfos();
+    std::size_t n = dev_infos.size();
+    if (n <= 0) {
+        QMessageBox::warning(nullptr,
+                             tr("ERROR"),
+                             tr("No MYNT@ EYE Depth Camera detected"),
+                             QMessageBox::Ok);
+
+        return false;
+    } else {
+        if (n == 1) {
+            *dev_info = dev_infos[0];
+            return true;
+        } else { // > 1
+//            QStringList items;
+//            for (auto &&info : dev_infos) {
+//                items << QString("%1 | %2").arg(info.index).arg(info.name.c_str());
+//            }
+
+//            bool ok;
+//            QString item = QInputDialog::getItem(
+//                this, tr("Select"), tr("MYNT® EYE cameras"), items, 0, false, &ok);
+//            if (ok) {
+//                QStringList parts = item.split("|");
+//                if (parts.length() > 0) {
+//                    QString index = parts[0].trimmed();
+//                    for (auto &&info : dev_infos) {
+//                        if (QString::number(info.index) == index) {
+//                            *dev_info = info;
+//                            return true;
+//                        }
+//                    }
+//                }
+//            }
+        }
+    }
+    return false;
+}
--- myntreader.h
+++ myntreader.h
@@ -5,6 +5,16 @@
 
 #include <QThread>
 
+#include <QMessageBox>
+
+
+#include <mynteyed/camera.h>
+#include <mynteyed/utils.h>
+
+#include <datasetwriter.h>
+
+MYNTEYE_USE_NAMESPACE
+
 /**
  * @brief The MYNTReader class
  * Simply read all stream data and motion information data.
@@ -14,6 +24,13 @@ class MYNTReader : public QThread
     Q_OBJECT
 public:
     explicit MYNTReader();
+
+private:
+    Camera mynt_cam_;
+
+    DatasetWriter *writher_ptr_;
+
+    bool selectDevice(DeviceInfo *dev_info);
 
 signals:
 
